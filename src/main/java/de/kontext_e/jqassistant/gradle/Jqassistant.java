bd106417548e0353e1ce5512de12e56df63454dd
WilliamThimm <William.Thimm@gmail.com>
2024-11-12 14:12:08 +0100

    Added arguments from build.gradle to command

--- src/main/java/de/kontext_e/jqassistant/gradle/Jqassistant.java
+++ src/main/java/de/kontext_e/jqassistant/gradle/Jqassistant.java
@@ -1,12 +1,11 @@
 package de.kontext_e.jqassistant.gradle;
 
-import org.gradle.api.logging.Logger;
-import org.gradle.api.logging.Logging;
 import org.gradle.api.tasks.Exec;
 import org.gradle.api.tasks.TaskAction;
 
+import java.util.StringJoiner;
+
 public class Jqassistant extends Exec {
-    private static final Logger LOGGER = Logging.getLogger(Jqassistant.class);
 
     private String taskName = "help";
     private JqassistantPluginExtension extension;
@@ -16,12 +15,36 @@ public class Jqassistant extends Exec {
     @Override
     @TaskAction
     public void exec() {
-        commandLine(extension.getInstallLocation() + "/bin/jqassistant.cmd", taskName);
+        StringJoiner command = new StringJoiner(" ");
+
+        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+            command.add("." + extension.getInstallLocation() + "/bin/jqassistant.cmd");
+        } else {
+            command.add("." + extension.getInstallLocation() + "/bin/jqassistant.sh");
+        }
+
+        command.add(taskName);
+
+        if (!extension.getConfigFiles().isEmpty()) {
+            command.add("-C");
+            extension.getConfigFiles().forEach(command::add);
+        }
+
+        if (!extension.getScanDirs().isEmpty()) {
+            command.add("-f");
+            extension.getScanDirs().forEach(command::add);
+        }
+
+        if (!extension.getScanUrls().isEmpty()){
+            command.add("-u");
+            extension.getScanUrls().forEach(command::add);
+        }
+
+        commandLine((Object[]) command.toString().split(" "));
         super.exec();
     }
 
     /* Setters */
-
     void addTaskName(String taskName) {
         this.taskName = taskName;
     }
